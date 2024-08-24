a5a4f97bdc8799af0ccc46e8a9d73871b710281d
unknown <rajsriselvan.ca@gmail.com>
2024-08-24 21:58:56 +0100

    file name change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
-import Technologies from './components/technologies';
+import Technologies from './components/Technologies';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
